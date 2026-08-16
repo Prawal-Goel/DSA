86df3f62a122576ac97ed16a22dad76be2c0cc93
Prawal-Goel <goelprawal@gmail.com>
2026-08-16 18:29:44 +0000

    Number of Good Pairs

---
+++ Arrays/Easy/Leet_1512.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 0);
+            }
+            count+=map.get(nums[i]);
+        }
+        return count;
+    }
+}
