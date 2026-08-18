8d31e1c8218e91c7a9dbb5a3ddcb94d3a9a7883f
Prawal-Goel <goelprawal@gmail.com>
2026-08-18 18:20:24 +0000

    Transpose Matrix

---
+++ Arrays/Easy/Leet_867.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int [][] nums= new int[matrix[0].length][matrix.length];
+        for (int i=0;i<matrix[0].length;i++){
+            for (int j=0;j<matrix.length;j++){
+                   nums[i][j]=matrix[j][i];
+            }
+        }
+        return nums;
+    }
+}
