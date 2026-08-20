e86574a584c7d57a8ea729c98e883ebf887d7e4e
Prawal-Goel <goelprawal@gmail.com>
2026-08-20 18:34:48 +0000

    Guess Number Higher or Lower

---
+++ Searching/Easy/Leet_374.java
@@ -0,0 +1,19 @@
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start=1;
+        int end=n;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int g=guess(mid);
+            if(g==-1){
+                end=mid-1;
+            }
+            else if(g==1){
+                start=mid+1;
+            }
+            else 
+            return mid;
+        }
+        return -1;
+    }
+}
