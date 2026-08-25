8470cd9617083fd340ac76b335810a3d297fdec9
Prawal-Goel <goelprawal@gmail.com>
2026-08-25 09:10:57 +0000

    Intersection of Two Arrays II

---
+++ Searching/Easy/Leet_350.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap <Integer,Integer> map = new HashMap<>();
+        ArrayList <Integer> ans =new ArrayList<>();
+        int N=0;
+        int n=0;
+        for (int i=0;i<nums1.length;i++){
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
+        }
+        for(int i=0;i<nums2.length;i++){
+            if(map.containsKey(nums2[i])){
+                if(map.get(nums2[i])>0){
+                    map.put(nums2[i],map.get(nums2[i])-1);
+                    ans.add(nums2[i]);
+                }
+            }
+        }
+        int []result= new int[ans.size()];
+        for (int i=0;i<ans.size();i++){
+            result[i]=ans.get(i);
+        }
+        return result;
+    }
+}
